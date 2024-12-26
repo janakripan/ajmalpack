c02280da53705a831339c6f08d22dcd984afa95e
janakripan <janakripanjkc@gmail.com>
2024-12-26 13:59:27 +0530

    4th commit

--- src/routes/components/Hero.jsx
+++ src/routes/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
   const headingRef = useRef()
   const divRef = useRef()
   const paraRef = useRef()
-
+  const buttonRef=useRef()
 
   useGSAP(()=>{
     const tl = gsap.timeline()
@@ -32,6 +32,13 @@ function Hero() {
       duration:.5,
       opacity:0,
     })
+    .from(buttonRef.current,{
+
+      y:30,
+      duration:.5,
+      opacity:0,
+
+    })
   })
 
   
@@ -42,14 +49,22 @@ function Hero() {
 
         <AutomaticSlider />
 
-        <div ref={divRef} className='p-6  bg-blur w-10/12 lg:w-3/12 absolute  lg:top-16  xl:top-24 lg:right-16 rounded-lg backdrop-blur-lg '>
-        <h1 ref={headingRef} className='text-white font-bold text-2xl md:text-3xl lg:text-4xl leading-relaxed tracking-wider '>
+        <div ref={divRef} className='p-6  bg-blur w-8/10 lg:w-3/12 absolute  lg:top-16 mx-4  xl:top-24 lg:right-16 rounded-lg backdrop-blur-lg '>
+        <h1 ref={headingRef} className='text-white font-bold text-3xl md:text-4xl lg:text-5xl  tracking-wider '
+        style={{lineHeight:'65px'}}>
         Explore the <br /> eco friendly Packing <br /> Products
         </h1>
         <p ref={paraRef} className='font-medium text-base md:text-md lg:text-lg text-white tracking-wide my-2 mb-6 lg:mb-8 xl:mb-10   '>
         The latest, greatest & freshest 
         from your fave brands.
         </p>
+        <div className="w-full flex justify-end ">
+          <div ref={buttonRef} className=' bg-[#555135] p-1 px-3 w-fit rounded-lg'>
+            <button className='w-full h-full text-white'>
+              Shop Now
+            </button>
+          </div>
+        </div>
         </div>
 
 
